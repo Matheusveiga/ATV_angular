470ed54f41e8b30c89bd3c027ba375b9f0a33388
Kaina <kaina.vieira@hotmail.com>
2024-12-04 16:53:53 -0300

    alteração no title, adicionei uma imagem como sugestão de logo pra avaliação, inserção do logo no favicon, inserção do formulario de cadastro, e importação do modulo reactivemodules para possível modificação do cadastro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,14 @@ import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import { CommonModule, NgFor } from '@angular/common';
 import { BannercarroselComponent } from "./components/bannercarrosel/bannercarrosel.component";
+import { ContainerCadastroComponent } from './components/container-cadastro/container-cadastro.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, HeaderComponent, FooterComponent, BannercarroselComponent, CommonModule],
+  imports: [RouterOutlet, HomeComponent, HeaderComponent, FooterComponent, BannercarroselComponent, CommonModule, ContainerCadastroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
